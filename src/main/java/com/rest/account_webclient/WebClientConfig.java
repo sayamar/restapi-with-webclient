ab6ed8d7bb7be229a1425cf4e3836c87104b6ce3
unknown <maruthi.sayampu@gmail.com>
2021-12-05 14:29:00 +0800

    Enhaced web client by having external properties with client http parameters...recommended way of implemeting the webclient

--- src/main/java/com/rest/account_webclient/WebClientConfig.java
+++ src/main/java/com/rest/account_webclient/WebClientConfig.java
@@ -1,16 +1,25 @@
 package com.rest.account_webclient;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Slf4j
 @Configuration
 @ConfigurationProperties("account")
+@Getter
+@Setter
 public class WebClientConfig {
 
     private String host;
@@ -18,30 +27,30 @@ public class WebClientConfig {
     private Long globalTimeout;
     private Long requestTimeout;
 
-    @Bean
-    public WebClient webClient() {
-        return WebClient.builder().baseUrl("http://localhost:9001")
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
-    }
-
 //    @Bean
 //    public WebClient webClient() {
-//
-//        ClientHttpConnector connector = new ReactorClientHttpConnector(
-//                HttpClient
-//                        .create()
-//                        .responseTimeout(Duration.ofMillis(globalTimeout))
-//                        .doOnRequest((req, conn) -> {
-//                            log.debug("webClient - {}", req);
-//                            req.responseTimeout(Duration.ofMillis(requestTimeout));
-//                        })
-//        );
-//        return WebClient.builder()
-//                .baseUrl("http://" + host + ":" + port)
-//                .clientConnector(connector)
-//                .build();
-//
+//        return WebClient.builder().baseUrl("http://localhost:9001")
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).build();
 //    }
+
+    @Bean
+    public WebClient webClient() {
+
+        ClientHttpConnector connector = new ReactorClientHttpConnector(
+                HttpClient
+                        .create()
+                        .responseTimeout(Duration.ofMillis(globalTimeout))
+                        .doOnRequest((req, conn) -> {
+                            log.debug("webClient - {}", req);
+                            req.responseTimeout(Duration.ofMillis(requestTimeout));
+                        })
+        );
+        return WebClient.builder()
+                .baseUrl("http://" + host + ":" + port)
+                .clientConnector(connector)
+                .build();
+
+    }
 
 
 }
